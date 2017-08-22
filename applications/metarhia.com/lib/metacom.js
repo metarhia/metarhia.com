4433aa25138b5abcf211bd2cab585c781f89eb2b
Mykola Bilochub <nbelochub@gmail.com>
2017-08-23 01:59:52 +0300

    Add file download code checking

--- applications/metarhia.com/lib/metacom.js
+++ applications/metarhia.com/lib/metacom.js
@@ -9,6 +9,9 @@ api.metacom.errors = Object.assign(Object.create(null), {
   ERR_PREVIOUS_UPLOAD_NOT_FINISHED: 35
 });
 
+api.metacom.uploadFilePrefixLength = 2;
+api.metacom.uploadFileNameLength = 8;
+
 api.metacom.chats = new Map();
 
 api.metacom.leaveChat = (connection) => {
@@ -28,8 +31,23 @@ api.metacom.generateFileCode = () => {
   const ALPHA = ALPHA_UPPER + ALPHA_LOWER;
   const DIGIT = '0123456789';
   const ALPHA_DIGIT = ALPHA + DIGIT;
-  result[0] = api.common.generateKey(2, DIGIT);
-  result[1] = api.common.generateKey(2, DIGIT);
-  result[2] = api.common.generateKey(8, ALPHA_DIGIT);
+  const prefixLength = api.metacom.uploadFilePrefixLength;
+  const fileNameLength = api.metacom.uploadFileNameLength;
+  result[0] = api.common.generateKey(prefixLength, DIGIT);
+  result[1] = api.common.generateKey(prefixLength, DIGIT);
+  result[2] = api.common.generateKey(fileNameLength, ALPHA_DIGIT);
+  return result;
+};
+
+api.metacom.parseFileCode = (code) => {
+  const result = new Array(4);
+  const prefixLength = api.metacom.uploadFilePrefixLength;
+  result[0] = code.slice(0, prefixLength);
+  result[1] = code.slice(prefixLength, prefixLength * 2);
+  result[2] = code.slice(prefixLength * 2);
+  result[3] =
+    result[2].length === api.metacom.uploadFileNameLength &&
+    result[1].length === prefixLength &&
+    result[0].length === prefixLength;
   return result;
 };
