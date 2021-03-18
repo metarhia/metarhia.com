39a30d51b988fbe49fad69d36bd2e7c1bce2ffd9
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-03-19 01:15:55 +0200

    Update dependencies
    RP-URL: https://github.com/metarhia/metarhia.com/pull/113

--- application/config/server.js
+++ application/config/server.js
@@ -3,10 +3,20 @@
   balancer: 80,
   protocol: 'https',
   ports: [443],
-  timeout: 5000,
-  concurrency: 1000,
+  nagle: false,
+  timeouts: {
+    bind: 2000,
+    start: 30000,
+    stop: 5000,
+    request: 5000,
+    watch: 1000,
+  },
   queue: {
+    concurrency: 1000,
     size: 2000,
     timeout: 3000,
+  },
+  workers: {
+    pool: 0,
   },
 });
